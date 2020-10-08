c70b95aef80610f8545130666c0dbba26bcd8129
Gogee90 <dead69-not@yandex.ru>
2020-10-08 16:19:56 +0500

    now is shown as human readable foreign key

--- fake_api/urls.py
+++ fake_api/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('secret_page/', admin.site.urls),
     path('api/', include('shop.urls'))
 ]
--- shop/serializers.py
+++ shop/serializers.py
@@ -1,14 +1,23 @@
 from rest_framework import serializers
-from .models import ProductModel
+from .models import ProductModel, ProductCategory
 from django.contrib.auth.models import User
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+  class Meta:
+    model = ProductCategory
+    fields = ['category', 'description', 'image']
 
 
 class ProductSerializer(serializers.ModelSerializer):
   owner = serializers.ReadOnlyField(source='owner.username')
+  category = serializers.ReadOnlyField(source='category.category')
+  author = serializers.ReadOnlyField(source='author.username')
 
   class Meta:
     model = ProductModel
-    fields = ['id', 'title', 'description', 'price', 'author', 'author_id', 'category', 'created_date', 'owner']
+    fields = ['id', 'title', 'description', 'price', 'author', 'author_id', 'category', 'created_date', 'owner', 'image']
 
   def create(self, validated_data):
     return ProductModel.objects.create(**validated_data)
